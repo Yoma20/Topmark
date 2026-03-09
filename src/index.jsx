8890147cdd9714d5120e822db64c9ecbd120225e
Yoma20 <98313300+Yoma20@users.noreply.github.com>
2026-03-09 06:59:40 +0300

    Add AuthProvider to index.jsx
    Wrap the App component with AuthProvider for context management.

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
