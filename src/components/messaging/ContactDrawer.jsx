831b8378ff114ee048b9ff5d5731610e00c3cf1d
Yoma20 <98313300+Yoma20@users.noreply.github.com>
2026-05-11 13:49:36 +0300

    added messaging parts

--- src/components/messaging/ContactDrawer.jsx
+++ src/components/messaging/ContactDrawer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthContext from "../AuthContext";
+import AuthContext from "../../AuthContext";
 import { startConversation } from "../api/messaging"; // adjust import path as needed
 import "./ContactDrawer.scss";
 
