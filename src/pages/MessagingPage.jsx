00a2bfef396c5d2eb0d6912f2a1c44a1b5fc885f
Yoma20 <98313300+Yoma20@users.noreply.github.com>
2026-05-12 08:30:05 +0300

    changing Auth

--- src/pages/MessagingPage.jsx
+++ src/pages/MessagingPage.jsx
@@ -8,6 +8,7 @@ import "./MessagingPage.scss";
 
 export default function MessagingPage() {
   const { user: currentUser } = useContext(AuthContext);
+  console.log("MP render, user id:", currentUser?.id, "ts:", Date.now());
   const { convId } = useParams();
 
   const [conversations, setConversations] = useState([]);
