6a464e7612641bcc2fd69c3691bc6694c43f8d84
Yoma20 <98313300+Yoma20@users.noreply.github.com>
2026-05-19 16:20:01 +0300

    order 2

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../AuthContext";
 import { getOrders, submitWork } from "../../api/gigs";
 import { startConversation } from "../../api/messages";
 import "./Orders.scss";
